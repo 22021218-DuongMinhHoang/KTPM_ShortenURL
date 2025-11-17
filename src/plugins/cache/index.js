209fef6da43ffccc9602a426cd2c05acb33070ef
DuongMinhHoang <22021218@vnu.edu.vn>
2025-11-17 09:41:38 +0700

    Fix another bug hehe

--- src/plugins/cache/index.js
+++ src/plugins/cache/index.js
@@ -1,5 +1,5 @@
-module.exports = {
-  //meta: { priority: NUMBER, phase: 'read'|'write'|'both' },
-  register: async ({app}) => { /* optional middleware setup */ },
-  decorate: (service) => { /* optional: return wrapped service */ }
-}
+// module.exports = {
+//   //meta: { priority: NUMBER, phase: 'read'|'write'|'both' },
+//   register: async ({app}) => { /* optional middleware setup */ },
+//   decorate: (service) => { /* optional: return wrapped service */ }
+// }
